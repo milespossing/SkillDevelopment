34da974ee6a679378bfbc7e4d19c68e635d7db5e
Pascal\Miles <miles@possing.com>
2018-09-02 08:12:54 -0700

    Initial Push of TaskManagerASP

---
+++ TaskManagerASP/TaskManagerASP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TaskManagerASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
