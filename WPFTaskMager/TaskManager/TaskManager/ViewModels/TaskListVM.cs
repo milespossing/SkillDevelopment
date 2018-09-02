20a45bddf08dc4b831de3229327dc6a4a1d729ce
Pascal\Miles <miles@possing.com>
2018-09-01 17:52:16 -0700

    Adding blend to the project and getting set up for interaction triggers
    Suuuuper cool! Using interaction triggers to handle events without code behind. Forward WPF!

--- WPFTaskMager/TaskManager/TaskManager/ViewModels/TaskListVM.cs
+++ WPFTaskMager/TaskManager/TaskManager/ViewModels/TaskListVM.cs
@@ -1,4 +1,6 @@
 using System.Linq;
+using System.Windows;
+using System.Windows.Input;
 using TaskDAL.DAL;
 using TaskDAL.Models;
 
@@ -8,12 +10,18 @@ namespace TaskManager.ViewModels
     {
         public Task[] Tasks { get; set; }
         public Task SelectedTask { get; set; }
+        public ICommand TestCommand => new SimpleCommand(Test);
 
         public TaskListVM()
         {
             TaskContext context = new TaskContext();
             Tasks = context.Tasks.ToArray();
             OnPropertyChanged(nameof(Tasks));
+        }
+
+        private void Test()
+        {
+            MessageBox.Show("Test!");
         }
     }
 }
