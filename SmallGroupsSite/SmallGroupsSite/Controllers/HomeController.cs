98f371c2f561ac273c3df7f77f99a0a63e459f68
Pascal\Miles <miles@possing.com>
2018-09-08 17:03:37 -0700

    Making a fun small page to get my feet wet with javascript

--- SmallGroupsSite/SmallGroupsSite/Controllers/HomeController.cs
+++ SmallGroupsSite/SmallGroupsSite/Controllers/HomeController.cs
@@ -34,6 +34,11 @@ namespace SmallGroupsSite.Controllers
             return View();
         }
 
+        public IActionResult Benefits()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ SmallGroupsSite/SmallGroupsSite/wwwroot/js/PlottingScript.js
@@ -0,0 +1,6 @@
+﻿moneyTime = document.getElementById('moneyTimePlot');
+Plotly.plot(moneyTime, [
+    {
+        x: [1, 2, 3, 4, 5],
+        y: [1, 2, 3, 4, 5]
+    }]);
